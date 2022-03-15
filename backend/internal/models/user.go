077384416bd91c2cb6e8ab99ffd272ac5263a970
the_nza <the_nza@protonmail.com>
2022-03-15 13:55:53 +0300

    Add user model to app with validation and password hashing

---
+++ backend/internal/models/user.go
@@ -0,0 +1,40 @@
+package models
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID             string `json:"id" db:"id"`
+	Username       string `json:"username" db:"username"`
+	HashedPassword string `json:"-" db:"password"`
+	Email          string `json:"email" db:"email"`
+	IsAdmin        bool   `json:"is_admin" db:"is_admin"`
+}
+
+func (u User) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Username, validation.Required, validation.Length(8, 0)),
+		validation.Field(&u.HashedPassword, validation.Required),
+		validation.Field(&u.Email, validation.When(u.Email != "", validation.Required, is.Email)),
+	)
+}
+
+func (u *User) HashPassword(password string) error {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+
+	u.HashedPassword = string(hashedBytes)
+
+	return nil
+}
+
+func (u User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.HashedPassword), []byte(password))
+
+	return err == nil
+}
